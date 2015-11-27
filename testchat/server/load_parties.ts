e59cd1307bf77b4dda887740e18a0beee865b25b
iostash <iostashvxi@gmail.com>
2015-11-27 14:02:03 +0200

    adding removing party implemented

---
+++ testchat/server/load_parties.ts
@@ -0,0 +1,28 @@
+import {Parties} from 'collections/parties';
+
+export function loadParties() {
+	var parties: Array<Object>;
+	if (Parties.find().count() === 0) {
+		parties = [
+	        {
+	            'name': 'Dubstep-Free Zone',
+	            'description': 'Can we please just for an evening not listen to dubstep.',
+	            'location': 'Los Angeles'
+	        },
+	        {
+	            'name': 'All dubstep all the time',
+	            'description': 'Get it on!',
+	            'location': 'Palo Alto'
+	        },
+	        {
+	            'name': 'Savage lounging',
+	            'description': 'Leisure suit required. And only fiercest manners.',
+	            'location': 'San Francisco'
+	        }
+	    ];
+		
+		for (let party of parties) {
+			Parties.insert(party);
+		}
+	}
+};
